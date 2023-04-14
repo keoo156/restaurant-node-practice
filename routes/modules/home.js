c5ee3df2c6f2321ebdb28847887b6df7feab032a
keoo156 <someonewasthere75@gmail.com>
2023-04-14 13:44:45 +0800

    sort method

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -14,4 +14,40 @@ router.get("/",(req,res)=>{
     .catch(e=> console.log(e))
 })
 
+router.get("/name",(req,res)=>{
+    Restaurant.find()
+    .lean()
+    .sort({name:"desc"})
+    .collation({ locale: "zh"})
+    .then(rests =>{ 
+        res.render("index",{rests})
+    })
+    
+    .catch(e=> console.log(e))
+})
+
+router.get("/category",(req,res)=>{
+    Restaurant.find()
+    .lean()
+    .sort({category:"asc"})
+    .collation({ locale: "zh"})
+    .then(rests =>{ 
+        res.render("index",{rests})
+    })
+    
+    .catch(e=> console.log(e))
+})
+
+router.get("/location",(req,res)=>{
+    Restaurant.find()
+    .lean()
+    .sort({location:"asc"})
+    .collation({ locale: "zh"})
+    .then(rests =>{ 
+        res.render("index",{rests})
+    })
+    
+    .catch(e=> console.log(e))
+})
+
 module.exports = router
